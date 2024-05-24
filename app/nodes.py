1ece23251194bf54529a76e64aaca490079c2dd2
RutamBhagat <rutambhagat@gmail.com>
2024-05-24 22:54:02 +0530

    add nodes.py

--- app/nodes.py
+++ app/nodes.py
@@ -1,11 +1,18 @@
 from dotenv import load_dotenv, find_dotenv
-from langgraph.prebuilt import ToolExecutor
+from langgraph.prebuilt import ToolExecutor, tool_executor
 from app.state import AgentState
 from react import react_agent_runnable, tools
 
 _ = load_dotenv(find_dotenv())
+tool_executor = ToolExecutor(tools)
 
 
 def run_agent_reasoning_engine(state: AgentState):
     agent_outcome = react_agent_runnable.invoke(state)
     return {"agent_outcome": agent_outcome}
+
+
+def execute_tools(state: AgentState):
+    agent_action = state["agent_outcome"]
+    output = tool_executor.invoke(agent_action)
+    return {"intermediate_steps": [(agent_action, str(output))]}
